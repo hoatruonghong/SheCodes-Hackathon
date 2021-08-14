3836c9391c25b122c2970c5d17a9c9ee02559454
hoatruonghong <70649552+hoatruonghong@users.noreply.github.com>
2021-08-14 19:53:00 +0700

    thêm hàm trả về

--- Backend.js
+++ Backend.js
@@ -1,4 +1,4 @@
-class User{
+class Account{
     constructor(id, name, phone, the_needy, ready, user_type, longitude, latitude) {
         this.id=id;
         this.name=name;
@@ -37,7 +37,7 @@ class Bill{
     }
 }
 
-class Account{
+class User{
     constructor(id_user, username, pwd){
         this.id_user=id_user;
         this.username=username;
@@ -46,3 +46,76 @@ class Account{
 }
 
 //Tạo get set info cho account, gọi các hàm bên controller
+/*--------Account-----------*/
+function getID_User(account){
+    return account.id;
+}
+function getNameUser(account){
+    return account.name;
+}
+function getPhoneUser(account){
+    return account.phone;
+}
+function getNeedyUser(account){
+    return account.the_needy;
+}
+function getReadyUser(account){
+    return account.ready;
+}
+function getTypeUser(account){
+    return account.user_type;
+}
+function getLongitudeUser(account){
+    return account.longitude;
+}
+function getLatitudeUser(account){
+    return account.latitude;
+}
+/*---------Point--------- */
+function getIDPoint(point){
+    return point.id_point;
+}
+function getID_userPoint(point){
+    return point.id_user;
+}
+function getNeedyPoint(point){
+    return point.the_needy;
+}
+/*--------Item-----------*/
+function getIDItem(item){
+    return item.id_item;
+}
+function getNameItem(item){
+    return item.item_name;
+}
+function getCurrentNeedItem(item){
+    return item.current_need_quantity;
+}
+function getSupportedItem(item){
+    return item.supported_quantity;
+}
+function getMedicalFlagItem(item){
+    return item.medical_flag;
+}
+/*--------bill--------------*/
+function getID_bill(bill){
+    return bill.id_bill;
+}
+function getID_bill(bill){
+    return bill.id_helper;
+}
+function getID_bill(bill){
+    return bill.id_needy;
+}
+/*--------Account-----------*/
+function getUsernameAccount(user){
+    return user.username;
+}
+function getID_User(user){
+    return user.id_user;
+}
+function getPwdAccount(user){
+    return user.pwd;
+}
+
+
